7f14b1c2fb38ad74bfab956bd84fc8b482911ed7
GrittyB <74409416+GrittyB@users.noreply.github.com>
2023-01-26 20:49:17 +0800

    complete project 5

--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+// Categories button and now_categories
+const Categories = ({categories, filterItems}) => { 
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className='filter-btn'
+            key={index}
+            onClick={() => filterItems(category)} //event handler
+          >
+            {category}
+          </button>
+          )
+      })}
+
+    </div>
+  )
+}
 
 export default Categories;
